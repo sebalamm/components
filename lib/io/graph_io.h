d743c658c9dcf9b261c4df4601304ddf09518525
Sebastian <seba.lamm@gmail.com>
2021-02-02 11:50:06 +0100

    Added binary format reader.

--- lib/io/graph_io.h
+++ lib/io/graph_io.h
@@ -843,6 +843,266 @@ class GraphIO {
     // exit(1);
   }
 
+  template<typename GraphType>
+  static void ReadSortedBinaryFile(GraphType &g, 
+                                   Config &config, 
+                                   PEID rank, PEID size, const MPI_Comm &comm) {
+    std::string line;
+    std::string filename(config.input_file);
+
+    // open file for reading
+    std::ifstream in(filename.c_str(), std::ios::binary);
+
+    if (!in) {
+      std::cerr << "Error opening " << filename << std::endl;
+      exit(0);
+    }
+
+    VertexID number_of_global_vertices = 0;
+    EdgeID number_of_global_edges = 0;
+
+    // Jump to beginning and get number of global vertices and edges
+    in.seekg(0, std::ios::beg);
+    in.read(reinterpret_cast<char*>(&number_of_global_vertices), sizeof(VertexID));
+    in.seekg(sizeof(VertexID), std::ios::beg);
+    in.read(reinterpret_cast<char*>(&number_of_global_edges), sizeof(EdgeID));
+
+    config.n = number_of_global_vertices;
+    config.m = number_of_global_edges;
+
+    // Read the lines i*ceil(m/size) to (i+1)*floor(m/size) lines of that file
+    VertexID leftover_edges = number_of_global_edges % size;
+    VertexID number_of_local_edges = (number_of_global_edges / size)
+      + static_cast<VertexID>(rank < leftover_edges);
+    VertexID from = (rank * number_of_local_edges)
+      + static_cast<VertexID>(rank >= leftover_edges ? leftover_edges : 0);
+    VertexID to = from + number_of_local_edges - 1;
+
+    // Gather local edges
+    std::vector<std::pair<VertexID, VertexID>> edge_list(number_of_local_edges);
+    auto header_offset = sizeof(VertexID) + sizeof(EdgeID);
+    auto edge_offset = from * sizeof(VertexID) * 2;
+    auto edge_buffer_size = number_of_local_edges * sizeof(VertexID) * 2;
+    in.seekg(header_offset + edge_offset, std::ios::beg);
+    in.read(reinterpret_cast<char*>(&edge_list[0]), edge_buffer_size);
+
+    // Compute vertex ranges
+    VertexID first_vertex = std::numeric_limits<VertexID>::max();
+    VertexID last_vertex = 0;
+    std::pair<VertexID, VertexID> first_vertex_range 
+      = {std::numeric_limits<VertexID>::max(), std::numeric_limits<VertexID>::max()};
+    for (EdgeID e = 0; e < edge_list.size(); ++e) {
+      edge_list[e].first--;
+      edge_list[e].second--;
+
+      if (first_vertex == std::numeric_limits<VertexID>::max()) {
+        first_vertex = edge_list[e].first;
+      }
+      last_vertex = edge_list[e].first;
+
+      if (edge_list[e].first == first_vertex) {
+        if (first_vertex_range.first == std::numeric_limits<VertexID>::max()) {
+          first_vertex_range.first = edge_list[e].second;
+        }
+        first_vertex_range.second = edge_list[e].second;
+      }
+    }
+
+    // Determine local and ghost vertices
+    google::dense_hash_set<VertexID> ghost_vertices; 
+    ghost_vertices.set_empty_key(EmptyKey);
+    ghost_vertices.set_deleted_key(DeleteKey);
+
+    VertexID number_of_ghost_vertices 
+      = DetermineGhostVertices(edge_list, first_vertex, last_vertex, ghost_vertices);
+
+    VertexID number_of_local_vertices = last_vertex - first_vertex + 1;
+
+    if constexpr (std::is_same<GraphType, StaticGraphCommunicator>::value
+                  || std::is_same<GraphType, StaticGraph>::value) {
+      g.StartConstruct(number_of_local_vertices,
+                       ghost_vertices.size(),
+                       edge_list.size(),
+                       first_vertex); 
+    } else if constexpr (std::is_same<GraphType, DynamicGraphCommunicator>::value
+                  || std::is_same<GraphType, DynamicGraph>::value) {
+      g.StartConstruct(number_of_local_vertices, 
+                       ghost_vertices.size(), 
+                       number_of_global_vertices);
+    } else {
+      g.StartConstruct(number_of_local_vertices, 
+                       ghost_vertices.size(), 
+                       first_vertex); 
+    }
+
+    // Add vertices for dynamic graphs
+    if constexpr (std::is_same<GraphType, DynamicGraphCommunicator>::value
+                  || std::is_same<GraphType, DynamicGraph>::value) {
+      for (VertexID v = 0; v < number_of_local_vertices; v++) {
+          g.AddVertex(first_vertex + v);
+      }
+    }
+
+    // Initialize payloads for graphs with communicator 
+    if constexpr (std::is_same<GraphType, StaticGraphCommunicator>::value
+                  || std::is_same<GraphType, DynamicGraphCommunicator>::value
+                  || std::is_same<GraphType, SemidynamicGraphCommunicator>::value) {
+      for (VertexID v = 0; v < number_of_local_vertices; v++) {
+          g.SetVertexLabel(v, first_vertex + v);
+          g.SetVertexRoot(v, rank);
+      }
+    }
+
+    // Add datatype
+    MPI_Datatype MPI_COMP;
+    MPI_Type_vector(1, 4, 0, MPI_VERTEX, &MPI_COMP);
+    MPI_Type_commit(&MPI_COMP);
+
+    // Gather vertex distribution
+    std::vector<std::tuple<VertexID, VertexID, VertexID, VertexID>> vertex_dist(size);
+    std::tuple<VertexID, VertexID, VertexID, VertexID> local_dist(first_vertex, 
+                                                                  last_vertex, 
+                                                                  first_vertex_range.first, 
+                                                                  first_vertex_range.second);
+    MPI_Allgather(&local_dist, 1, MPI_COMP,
+                  &vertex_dist[0], 1, MPI_COMP, comm);
+
+    // Resulting mapping
+    google::dense_hash_map<VertexID, 
+                          std::vector<std::tuple<VertexID, 
+                                                 VertexID, 
+                                                 VertexID,
+                                                 PEID>>> vertex_ranges;
+    vertex_ranges.set_empty_key(EmptyKey);
+
+    // Construct mapping: v -> list<[first edge, last edge], duplicate ID, pe>
+    VertexID prev_range_last = std::get<1>(vertex_dist[0]);
+    VertexID prev_vertex_range_first = std::get<2>(vertex_dist[0]);
+    VertexID prev_vertex_range_last = std::get<3>(vertex_dist[0]);
+    VertexID prev_last_pe = 0;
+    for (PEID i = 1; i < vertex_dist.size(); ++i) {
+      VertexID current_range_first = std::get<0>(vertex_dist[i]);
+      VertexID current_range_last = std::get<1>(vertex_dist[i]);
+      VertexID current_vertex_range_first = std::get<2>(vertex_dist[i]);
+      VertexID current_vertex_range_last = std::get<3>(vertex_dist[i]);
+      if (prev_range_last == current_range_last
+          || (prev_range_last != current_range_last 
+            && prev_range_last == current_range_first)) {
+        if (vertex_ranges.find(prev_range_last) == vertex_ranges.end()) {
+          vertex_ranges[prev_range_last].emplace_back(0, current_vertex_range_first - 1, prev_range_last, prev_last_pe);
+          if (prev_last_pe != rank) {
+            ghost_vertices.erase(prev_range_last);
+            // std::cout << "R" << rank << " add ghost (dist gen init) v " << prev_range_last << " pe(v)=" << prev_last_pe << std::endl;
+            g.AddGhostVertex(prev_range_last, prev_last_pe);
+          }
+        }
+        VertexID duplicate_id = (4 * number_of_global_vertices) * (i + size);
+        vertex_ranges[prev_range_last].emplace_back(current_vertex_range_first, current_vertex_range_last, duplicate_id, i);
+        if (i != rank) {
+          // std::cout << "R" << rank << " add ghost (dist gen) v " << duplicate_id << " pe(v)=" << i << std::endl;
+          g.AddGhostVertex(duplicate_id, i);
+          if (prev_last_pe == rank) {
+            edge_list.emplace_back(duplicate_id, prev_range_last);
+            edge_list.emplace_back(prev_range_last, duplicate_id);
+          } 
+        } else {
+          if constexpr (std::is_same<GraphType, StaticGraphCommunicator>::value
+                        || std::is_same<GraphType, StaticGraph>::value
+                        || std::is_same<GraphType, SemidynamicGraphCommunicator>::value
+                        || std::is_same<GraphType, SemidynamicGraph>::value) {
+            // std::cout << "R" << rank << " add dupl v " << duplicate_id << " of " << prev_range_last << std::endl;
+            g.AddDuplicateVertex(prev_range_last, duplicate_id);
+          } else {
+            // std::cout << "R" << rank << " add local v " << duplicate_id << std::endl;
+            g.AddVertex(duplicate_id);
+          }
+          edge_list.emplace_back(duplicate_id, prev_range_last);
+          edge_list.emplace_back(prev_range_last, duplicate_id);
+        }
+      }
+      if (prev_range_last != current_range_last) {
+        prev_range_last = current_range_last;
+        prev_vertex_range_first = current_vertex_range_first;
+        prev_vertex_range_last = current_vertex_range_last;
+        prev_last_pe = i;
+      }
+    }
+
+    // Initialize ghost vertices
+    for (auto &v : ghost_vertices) {
+      // Get PE for ghost
+      PEID pe = rank;
+      for (PEID i = 0; i < vertex_dist.size(); ++i) {
+        if (v >= std::get<0>(vertex_dist[i]) && v <= std::get<1>(vertex_dist[i])) {
+          pe = i;
+        }
+      }
+      // std::cout << "R" << rank << " add ghost (regular) v " << v << " pe(v)=" << pe << std::endl;
+      g.AddGhostVertex(v, pe);
+    }
+
+    // Update edges with data from duplicates
+    for (auto &edge : edge_list) {
+      VertexID source = std::numeric_limits<VertexID>::max();
+      VertexID target = std::numeric_limits<VertexID>::max();
+      PEID target_pe = std::numeric_limits<PEID>::max();
+      // Find updated source
+      // TODO: Replace with proper binary search
+      if (vertex_ranges.find(edge.first) != vertex_ranges.end()) {
+        for (auto &range : vertex_ranges[edge.first]) {
+          VertexID first_edge = std::get<0>(range);
+          VertexID last_edge = std::get<1>(range);
+          VertexID dupl_id = std::get<2>(range);
+          PEID pe = std::get<3>(range);
+          if (edge.second >= first_edge && edge.second <= last_edge) {
+            source = dupl_id;
+            break;
+          }
+        }
+      }
+      // Find updated target
+      if (vertex_ranges.find(edge.second) != vertex_ranges.end()) {
+        for (auto &range : vertex_ranges[edge.second]) {
+          VertexID first_edge = std::get<0>(range);
+          VertexID last_edge = std::get<1>(range);
+          VertexID dupl_id = std::get<2>(range);
+          PEID pe = std::get<3>(range);
+          if (edge.first >= first_edge && edge.first <= last_edge) {
+            target = dupl_id;
+            target_pe = pe;
+            break;
+          }
+        }
+      }
+
+      // Properly update edge if not duplicates were found
+      if (source == std::numeric_limits<VertexID>::max()) 
+        source = edge.first;
+      if (target == std::numeric_limits<VertexID>::max())
+        target = edge.second;
+
+      // Update edge
+      edge.first = source;
+      edge.second = target;
+    }
+
+    // Sort edges for static graphs
+    if constexpr (std::is_same<GraphType, StaticGraphCommunicator>::value
+                  || std::is_same<GraphType, StaticGraph>::value) {
+      SortEdges<GraphType>(g, edge_list);
+    }
+
+    // Finally add edges
+    for (auto &edge : edge_list) {
+      g.AddEdge(g.GetLocalID(edge.first), edge.second, g.GetPE(g.GetLocalID(edge.second)));
+    }
+
+    g.FinishConstruct();
+    // g.OutputLocal();
+    // MPI_Barrier(MPI_COMM_WORLD);
+    // exit(1);
+  }
+
   static PEID GetPEFromOffset(const VertexID v, 
                               std::vector<std::pair<VertexID, VertexID>> offset_array,
                               PEID default_rank) {
